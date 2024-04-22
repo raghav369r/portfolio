42857aee7c8df31259191e8426b7c4b2dd301e67
raghav369r <raghav1010reddy@gmail.com>
2024-04-22 16:09:52 +0530

    added responsiveness to navbar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -2,23 +2,33 @@ import { useState } from "react";
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import logo from "../../assets/logo.svg";
 
+import { CiMenuFries } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
 
 export const Navbar = () => {
   const [page ,setPage]=useState("home");
+  const [opened,setOpened]=useState(false);
   // console.log(page);
   return (
-    <div className="flex justify-between items-center my-5 mx-32">
-      <img className="" src={logo} />
-      <ul className="flex gap-5 font-customFont">
-        <li onClick={()=>setPage("home")}><AnchorLink  href="#Home">Home</AnchorLink></li>
-        <li onClick={()=>setPage("about")}><AnchorLink  href="#about">About Us</AnchorLink></li>
-        <li onClick={()=>setPage("servoces")}><AnchorLink  href="#projects">Services</AnchorLink></li>
-        <li onClick={()=>setPage("projects")}><AnchorLink  href="#projects">Portfolio</AnchorLink></li>
-        <li onClick={()=>setPage("contact")}><AnchorLink  href="#contact">Contact</AnchorLink></li>
-      </ul>
+    <div className="flex justify-between items-center my-5 md:mx-32">
+      <img className="" src={logo}/>
+      <div className="relative md:hidden" onClick={()=>setOpened(!opened)}>
+        <CiMenuFries className="size-10 cursor-pointer"/>
+      </div>
+      <div className={"w-1/2 bg-black h-screen md:bg-transparent md:h-auto md:w-auto absolute top-0 right-0 md:relative md:block"+(opened?" block":" hidden")}>
+        <ul className={"flex flex-col justify-end items-center md:flex-row gap-5 sm:flex-col font-customFont "}>
+          <li className="md:hidden hover:border-b cursor-pointer ml-auto"><IoMdClose className="size-10 cursor-pointer" onClick={()=>setOpened(!opened)}/></li>
+          <li className="hover:border-b cursor-pointer" onClick={()=>setPage("home")}><AnchorLink  href="#Home">Home</AnchorLink></li>
+          <li className="hover:border-b cursor-pointer" onClick={()=>setPage("about")}><AnchorLink  href="#about">About Us</AnchorLink></li>
+          <li className="hover:border-b cursor-pointer" onClick={()=>setPage("services")}><AnchorLink  href="#projects">Services</AnchorLink></li>
+          <li className="hover:border-b cursor-pointer" onClick={()=>setPage("projects")}><AnchorLink  href="#projects">Portfolio</AnchorLink></li>
+          <li className="hover:border-b cursor-pointer" onClick={()=>setPage("contact")}><AnchorLink  href="#contact">Contact</AnchorLink></li>
+        </ul>
+      </div>
       <AnchorLink href="#contact" className="hidden md:block py-3 px-7 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 cursor-pointer rounded-3xl transition delay-150 hover:scale-105">
         Connect With Me
       </AnchorLink>
     </div>
   );
 };
+
